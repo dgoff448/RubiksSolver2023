c59a04b31cb1c645f8c83364919782967f31835e
David Goff <dgoff448@students.bju.edu>
2023-12-15 16:57:41 -0500

    swap and push cube methods

--- cube.py
+++ cube.py
@@ -1,3 +1,99 @@
 class Cube:
     def __init__(self, cells):
         self.cells = cells
+    
+    def swap(self, a: int, b: int, c: int, d: int):
+        if a < c and b < d:
+            arr = self.cells
+            s1 = arr[0:a]
+            s2 = arr[a:b]
+            s3 = arr[b:c]
+            s4 = arr[c:d]
+            s5 = arr[d:len(arr)]
+        else:
+            s1 = arr[0:c]
+            s2 = arr[c:d]
+            s3 = arr[d:a]
+            s4 = arr[a:b]
+            s5 = arr[b:len(arr)]
+        
+        # print(s1, s2, s3, s4, s5)
+        self.cells = s1 + s4 + s3 + s2 + s5
+    
+    def push(a, b, c, d):
+        if a < c and b < d:
+            s1 = arr[0:a]
+            s2 = arr[a:b]
+            s3 = arr[b:c]
+            s4 = arr[c:d]
+            s5 = arr[d:len(arr)]
+
+            print(s1, s2, s3, s4, s5)
+            self.cells = s1 + [-1]*len(s2) + s3 + s2 + s5
+            return s4
+        else:
+            s1 = arr[0:c]
+            s2 = arr[c:d]
+            s3 = arr[d:a]
+            s4 = arr[a:b]
+            s5 = arr[b:len(arr)]
+
+            print(s1, s2, s3, s4, s5)
+            self.cells = s1 + s4 + s3 + [-1]*len(s4) + s5
+            return s2
+
+
+    def white_CW(self):
+        self.swap(1, 7, 7, 9) # rotating white cells CW
+        self.swap()
+
+    def white_CCW(self):
+        self.swap(1, 3, 3, 9) # rotating white cells CCW
+
+    def white_Dub(self):
+        self.swap(1, 5, 5, 9) # rotating white cells Double
+
+    def blue_CW(self):
+        pass 
+
+    def blue_CCW(self):
+        pass 
+
+    def blue_Dub(self):
+        pass 
+    
+    def red_CW(self):
+        pass 
+
+    def red_CCW(self):
+        pass 
+
+    def red_Dub(self):
+        pass 
+
+    def green_CW(self):
+        pass 
+
+    def green_CCW(self):
+        pass 
+
+    def green_Dub(self):
+        pass 
+
+    def orange_CW(self):
+        pass 
+
+    def orange_CCW(self):
+        pass 
+
+    def orange_Dub(self):
+        pass 
+
+    def yellow_CW(self):
+        pass 
+
+    def yellow_CCW(self):
+        pass 
+
+    def yellow_Dub(self):
+        pass 
--- test.py
+++ test.py
@@ -1,20 +1,49 @@
-cornerConnectionCells = [
-    ['W1', 'B3', 'R7'],
-    ['W3', 'R5', 'G1'],
-    ['W5', 'G7', 'O3'],
-    ['W7', 'O1', 'B5'],
-    ['Y1', 'B7', 'O7'],
-    ['Y3', 'O5', 'G5'],
-    ['Y5', 'R3', 'G3'],
-    ['Y7', 'B1', 'R1'],
-]
+def swapSlices(arr, a, b, c, d):
+    if a < c and b < d:
+        s1 = arr[0:a]
+        s2 = arr[a:b]
+        s3 = arr[b:c]
+        s4 = arr[c:d]
+        s5 = arr[d:len(arr)]
+    else:
+        s1 = arr[0:c]
+        s2 = arr[c:d]
+        s3 = arr[d:a]
+        s4 = arr[a:b]
+        s5 = arr[b:len(arr)]
 
-for cc in cornerConnectionCells:
-    print("'" + cc[0][0] + cc[1][0] + cc[2][0] + "'", ":", "'" + cc[0] + "',")
-    print("'" + cc[0][0] + cc[2][0] + cc[1][0] + "'", ":", "'" + cc[0] + "',")
+    print(s1, s2, s3, s4, s5)
+    return  s1 + s4 + s3 + s2 + s5
 
-    print("'" + cc[1][0] + cc[0][0] + cc[2][0] + "'", ":", "'" + cc[1] + "',")
-    print("'" + cc[1][0] + cc[2][0] + cc[0][0] + "'", ":", "'" + cc[1] + "',")
 
-    print("'" + cc[2][0] + cc[1][0] + cc[0][0] + "'", ":", "'" + cc[2] + "',")
-    print("'" + cc[2][0] + cc[0][0] + cc[1][0] + "'", ":", "'" + cc[2] + "',")
+def push(arr, a, b, c, d):
+    if a < c and b < d:
+        s1 = arr[0:a]
+        s2 = arr[a:b]
+        s3 = arr[b:c]
+        s4 = arr[c:d]
+        s5 = arr[d:len(arr)]
+
+        print(s1, s2, s3, s4, s5)
+        return [s1 + [-1]*len(s2) + s3 + s2 + s5, s4]
+    else:
+        s1 = arr[0:c]
+        s2 = arr[c:d]
+        s3 = arr[d:a]
+        s4 = arr[a:b]
+        s5 = arr[b:len(arr)]
+
+        print(s1, s2, s3, s4, s5)
+        return [s1 + s4 + s3 + [-1]*len(s4) + s5, s2]
+
+    
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(arr)
+print(swapSlices(arr, 0, 1, 9, 10))
+print()
+print(arr)
+print(swapSlices(arr, 9, 10, 0, 1))
+
+# print(push(arr, 1,3,6,8))
+# print()
+# print(push(arr, 6,8,1,3))
